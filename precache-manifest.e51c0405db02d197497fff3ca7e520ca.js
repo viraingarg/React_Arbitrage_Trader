d99b2e9f64f3cc4eb19045b481310d5c8eb55b54
Virain Garg <virain.garg@ad.infosys.com>
2019-06-01 20:43:37 +0530

    Updates

---
+++ precache-manifest.e51c0405db02d197497fff3ca7e520ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b551be810fc79e48926d93e03c0223",
+    "url": "/React_Arbitrage_Trader/index.html"
+  },
+  {
+    "revision": "03b3eaa629c94f4e5d5f",
+    "url": "/React_Arbitrage_Trader/static/css/2.de53ba08.chunk.css"
+  },
+  {
+    "revision": "484d877c2f1db6cc87b6",
+    "url": "/React_Arbitrage_Trader/static/css/main.818efdbb.chunk.css"
+  },
+  {
+    "revision": "03b3eaa629c94f4e5d5f",
+    "url": "/React_Arbitrage_Trader/static/js/2.6eaad578.chunk.js"
+  },
+  {
+    "revision": "484d877c2f1db6cc87b6",
+    "url": "/React_Arbitrage_Trader/static/js/main.a9dd3a81.chunk.js"
+  },
+  {
+    "revision": "1a81986384424f6b15ee",
+    "url": "/React_Arbitrage_Trader/static/js/runtime~main.39b8580f.js"
+  }
+]);
